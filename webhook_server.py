5e7a1ce8350299f8d04ca6f740e37093975d4f9a
Murillospurio <murillospuriopiraju@gmail.com>
2025-04-22 17:52:19 -0300

    Substitui ngrok por solução de túnel estático para garantir estabilidade

--- webhook_server.py
+++ webhook_server.py
@@ -87,4 +87,4 @@ def buscar_detalhes_pagamento(payment_id):
         return None
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    aapp.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
